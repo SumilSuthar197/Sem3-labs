af67deb3369e5edb5b9964aa84f344cd92980533
SumilSuthar197 <sumil.suthar@gmail.com>
2022-11-09 07:08:50 +0530

    adding haskell program

---
+++ PCPF/Haskell/factorail.hs
@@ -0,0 +1,15 @@
+fact:: Int->Int
+fact n|n==0=1
+fact n|n/=0=n*fact(n-1)
+
+main = do
+       putStrLn("Enter the Number")
+       ain <- getLine
+       let a =(read ain :: Int)
+       putStrLn("Factorial of the number is:")
+       print(fact a)
+    
+    
+    
+    
+  
